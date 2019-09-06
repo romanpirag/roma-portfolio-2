f5c2332938d863a78e1a1cfab5aeba0f793af8fd
Roman Pirag <romanpirag@gmail.com>
2019-09-06 15:16:14 -0400

    Removes Articles and Changes CSS

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -3,62 +3,57 @@ import React from "react"
 class Blogs extends React.Component {
   render() {
     return (
-      <article>
-        <div className="blogs-container">
-          <div className="divider">
-            <div>Technical Blogs</div>
-          </div>
-          <h2 className="projects-header">Technical Blogs</h2>
+      <div className="blogs-container">
+        <div className="divider">Technical Blogs</div>
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/@romabot/code-zombie-1-intro-to-classes-ruby-6d0d56372ee5"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/5Sz1VMF.png"
-              alt="Outer Brain Website"
-            />
-          </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/@romabot/code-zombie-1-intro-to-classes-ruby-6d0d56372ee5"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/5Sz1VMF.png"
+            alt="Outer Brain Website"
+          />
+        </a>
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/@romabot/code-zombie-2-how-to-avoid-burnout-survive-the-learning-curve-efc32b8efdb5"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/RM2yGiR.png"
-              alt="Outer Brain Website"
-            />
-          </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/@romabot/code-zombie-2-how-to-avoid-burnout-survive-the-learning-curve-efc32b8efdb5"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/RM2yGiR.png"
+            alt="Outer Brain Website"
+          />
+        </a>
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/@romabot/code-zombie-3-css-for-the-braindead-part-1-5657843a42ec"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/tNBSRru.png"
-              alt="Outer Brain Website"
-            />
-          </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/@romabot/code-zombie-3-css-for-the-braindead-part-1-5657843a42ec"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/tNBSRru.png"
+            alt="Outer Brain Website"
+          />
+        </a>
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/@romabot/code-zombie-4-css-for-the-braindead-part-2-17bb71c74396"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/sNeDsJN.png"
-              alt="Outer Brain Website"
-            />
-          </a>
-        </div>
-      </article>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/@romabot/code-zombie-4-css-for-the-braindead-part-2-17bb71c74396"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/sNeDsJN.png"
+            alt="Outer Brain Website"
+          />
+        </a>
+      </div>
     )
   }
 }
--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -3,89 +3,63 @@ import React from "react"
 class ProjectsPage extends React.Component {
   render() {
     return (
-      <article>
         <div className="project-container">
-          <div className="divider">
-            <div>Development</div>
-          </div>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://neonflix.netlify.com/"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/1lhVqD7.png"
-              alt="Habitrack"
-            />
-          </a>
-          {/* <p className="project-description">
-            <strong>Habitrack:</strong>
-            <span className="inner-description">
-              Neon city inspired skill tracker app
-            </span>
-            <span className="small-app-description">Skill tracker app</span>
-          </p> */}
-          <h2 className="projects-header">Development</h2>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://habitrack.netlify.com/"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/i4DUMH0.png"
-              alt="Habitrack"
-            />
-          </a>
-          {/* <p className="project-description">
-            <strong>Habitrack:</strong>
-            <span className="inner-description">
-              Neon city inspired skill tracker app
-            </span>
-            <span className="small-app-description">Skill tracker app</span>
-          </p> */}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://outerbrainstudios.netlify.com/"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/H4gmm4Q.png"
-              alt="Outer Brain Website"
-            />
-          </a>
-          {/* <p className="project-description">
-            <strong>Outer Brain:</strong>
-            <span className="inner-description">
-              Website for a Video Game Development Studio
-            </span>
-            <span className="small-app-description">
-              Videogame development studio
-            </span>
-          </p> */}
+        
+            <div className="divider">Development</div>
+          
+          
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://soundscape.netlify.com/"
-          >
-            <img
-              className="project-link"
-              src="https://i.imgur.com/DX8UKNe.png"
-              alt="SoundScape"
-            />
-          </a>
-          {/* <p className="project-description">
-            <strong>Soundscape:</strong>
-            <span className="inner-description">
-              Journal app which uses sound instead of words
-            </span>
-            <span className="small-app-description">Sound journal app</span>
-          </p> */}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://neonflix.netlify.com/"
+            >
+              <img
+                className="project-link"
+                src="https://i.imgur.com/1lhVqD7.png"
+                alt="Habitrack"
+              />
+            </a>
+
+            <h2 className="projects-header">Development</h2>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://habitrack.netlify.com/"
+            >
+              <img
+                className="project-link"
+                src="https://i.imgur.com/i4DUMH0.png"
+                alt="Habitrack"
+              />
+            </a>
+
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://outerbrainstudios.netlify.com/"
+            >
+              <img
+                className="project-link"
+                src="https://i.imgur.com/H4gmm4Q.png"
+                alt="Outer Brain Website"
+              />
+            </a>
+
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://soundscape.netlify.com/"
+            >
+              <img
+                className="project-link"
+                src="https://i.imgur.com/DX8UKNe.png"
+                alt="SoundScape"
+              />
+            </a>
+      
+        
         </div>
-      </article>
     )
   }
 }
