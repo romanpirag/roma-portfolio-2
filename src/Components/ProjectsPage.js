cd24896d68f843c43fbf93eb3ae03af61cc736d4
Roman Pirag <romanpirag@gmail.com>
2019-09-09 23:29:18 -0400

    Changes positions of projects

--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -3,60 +3,58 @@ import React from "react"
 class ProjectsPage extends React.Component {
   render() {
     return (
-        <div className="project-container">
-        
-            <div className="divider">Development</div>
-          
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://neonflix.netlify.com/"
-            >
-              <img
-                className="project-link"
-                src="https://i.imgur.com/1lhVqD7.png"
-                alt="Habitrack"
-              />
-            </a>
+      <div className="project-container">
+        <div className="divider">Development</div>
 
-            <h2 className="projects-header">Development</h2>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://habitrack.netlify.com/"
-            >
-              <img
-                className="project-link"
-                src="https://i.imgur.com/i4DUMH0.png"
-                alt="Habitrack"
-              />
-            </a>
+        <h2 className="projects-header">Development</h2>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://habitrack.netlify.com/"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/i4DUMH0.png"
+            alt="Habitrack"
+          />
+        </a>
 
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://outerbrainstudios.netlify.com/"
-            >
-              <img
-                className="project-link"
-                src="https://i.imgur.com/H4gmm4Q.png"
-                alt="Outer Brain Website"
-              />
-            </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://outerbrainstudios.netlify.com/"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/H4gmm4Q.png"
+            alt="Outer Brain Website"
+          />
+        </a>
 
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://soundscape.netlify.com/"
-            >
-              <img
-                className="project-link"
-                src="https://i.imgur.com/DX8UKNe.png"
-                alt="SoundScape"
-              />
-            </a>
-      
-        </div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://neonflix.netlify.com/"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/1lhVqD7.png"
+            alt="Habitrack"
+          />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://soundscape.netlify.com/"
+        >
+          <img
+            className="project-link"
+            src="https://i.imgur.com/DX8UKNe.png"
+            alt="SoundScape"
+          />
+        </a>
+      </div>
     )
   }
 }
